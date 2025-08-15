666e262a89333c85dfde0de23c388bdf505557c1
ricloy <ricloy@users.noreply.github.com>
2025-08-15 01:10:02 +0000

    Update update.sh
    add missing single quote

--- src/update.sh
+++ src/update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -s 'https://planet.openstreetmap.org/statistics/data_stats.html' | sed '/Top 50 users for uploads of GPS data/,/Number of users editing over the past/{/Number of users editing over the past/!d};/Top users editing/,/Report took/{/Report took/!d}' >./out/report_$(date +'%Y-%m-%d).html
+curl -s 'https://planet.openstreetmap.org/statistics/data_stats.html' | sed '/Top 50 users for uploads of GPS data/,/Number of users editing over the past/{/Number of users editing over the past/!d};/Top users editing/,/Report took/{/Report took/!d}' >./out/report_$(date +'%Y-%m-%d').html
